68b03ffea9310ac721bd2850852ccfef15bb1f0a
Alexander Kislov <strannik11.92@gmail.com>
2026-08-19 17:36:10 +0300

    chore: remove bundled xray plugin

--- xray.koplugin/_meta.lua
+++
@@ -1,9 +0,0 @@
--- Plugin metadata
-local _ = require("gettext")
-
-return {
-    name = "xray",
-    fullname = _("X-Ray"),
-    description = _([[Transform your reading experience with AI-powered book analysis, just like Amazon Kindle X-Ray!]]),
-    version = "26.8.1-beta"
-}
